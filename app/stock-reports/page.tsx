04624b11cacde4ae72ec8e79a50d3d4724f9f3d5
given1324 <given@student.pradita.ac.id>
2026-04-07 15:03:42 +0700

    Add files via upload

---
+++ app/stock-reports/page.tsx
@@ -0,0 +1,9 @@
+import StockReportSection from "@/components/StockReportSection";
+
+export default function StockReportsPage() {
+  return (
+    <div className="w-full">
+      <StockReportSection />
+    </div>
+  );
+}
