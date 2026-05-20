89bc0b895e7056a7449951286607faa04c0d34fb
Jonatan345 <imanuel.jonatan@student.pradita.ac.id>
2026-05-20 09:25:13 +0700

    Add files via upload

---
+++ app/categories/page.tsx
@@ -0,0 +1,9 @@
+import CategorySection from "@/components/CategorySection";
+
+export default function CategoriesPage() {
+  return (
+    <div className="space-y-6">
+      <CategorySection />
+    </div>
+  );
+}
